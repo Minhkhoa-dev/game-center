65b9514a942b6d442ef23a1fad36fff41999d5f4
Minhkhoa-dev <minhnguyen101920@gmail.com>
2025-03-05 00:29:43 +0700

    cicd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
                     <img src={reactLogo} className='logo react' alt='React logo' />
                 </a>
             </div>
-            <h1>Minh Khang Khoa</h1>
+            <h1>Minh-Khang-Khoa-Tuyết</h1>
             <div className='card'>
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
